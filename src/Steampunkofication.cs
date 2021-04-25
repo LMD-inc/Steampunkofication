c11db6b92d757e6e040daa7a7d4133a7e601a8a2
Alexander Malinin <podhvatovshurik@gmail.com>
2021-04-26 01:55:08 +0300

    Init project structure

---
+++ src/Steampunkofication.cs
@@ -0,0 +1,29 @@
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+
+[assembly: ModInfo("Steampunkofication",
+  Description = "Steampunk. More.",
+  Website = "https://github.com/LMD-inc/Steampunkofication",
+  Authors = new[] { "simplewepro" })]
+
+namespace Steampunkofication
+{
+  public class SteampunkoficationMod : ModSystem
+  {
+    public override void Start(ICoreAPI api)
+    {
+
+    }
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+
+    }
+  }
+}
