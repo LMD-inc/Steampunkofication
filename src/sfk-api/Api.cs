6cc03f35ffc5f9a8af7b406adb6e24393eca174d
Alexander <podhvatovshurik@gmail.com>
2022-09-05 13:04:11 +0300

    🔖 upgrade to v0.1.2

--- src/sfk-api/Api.cs
+++ src/sfk-api/Api.cs
@@ -4,7 +4,7 @@ using Vintagestory.API.Server;
 
 [assembly: ModInfo("[SFK] Api",
   Description = "Steampunk. More. It's based.",
-  Version = "0.1.1",
+  Version = "0.1.2",
   Website = "https://github.com/LMD-inc/Steampunkofication",
   Authors = new[] { "LMD inc." })]
 
